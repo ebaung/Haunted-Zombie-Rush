f1ea3f1b3e0fdcc9a2685a6ca9c64554d12004c6
Eugene Baung <ebaung@gmail.copm>
2018-01-23 20:21:41 +0900

    Haunted Zombie Rush [Unity 3D] initial commit [L1.26: Game state & singletons in Unity 3D]

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] private float jumpForce = 100f;
+    [SerializeField] private AudioClip sfxJump;
+    [SerializeField] private AudioClip sfxDeath;
+
+
+	private Animator anim;
+	private Rigidbody rigidBody;
+	private bool jump = false;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        Assert.IsNotNull(sfxJump);
+        Assert.IsNotNull(sfxDeath);
+    }
+
+    // Use this for initialization
+    void Start () {
+		anim = GetComponent<Animator> ();
+		rigidBody = GetComponent<Rigidbody> ();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!GameManager.instance.GameOver){
+
+			if (Input.GetMouseButtonDown (0)) {		// 0 = left click, 1 = right click
+				GameManager.instance.PlayerStartedGame();
+				anim.Play("Jump");
+            	audioSource.PlayOneShot(sfxJump);
+				rigidBody.useGravity = true;
+				jump = true;
+			}
+		}
+	}
+
+	void FixedUpdate(){
+		if (jump == true) {
+			jump = false;
+			rigidBody.velocity = new Vector2 (0 , 0);
+			rigidBody.AddForce (new Vector2 (0, jumpForce), ForceMode.Impulse);
+		}
+		//print (rigidBody.velocity.y);
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "obstacle")
+        {
+            rigidBody.AddForce(new Vector2 (-50,20), ForceMode.Impulse);
+            rigidBody.detectCollisions = false;
+            audioSource.PlayOneShot(sfxDeath);
+			GameManager.instance.PlayerCollided ();
+        }
+    }
+}
---
+++ Assets/Scripts/move_platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_platform : MonoBehaviour {		// Mark Price named this script "Object.cs"
+
+	[SerializeField] private float objectSpeed = 1;
+	[SerializeField] private float resetPosition = -45.4f;
+	[SerializeField] private float startPosition = 46.9f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () {
+
+		if (!GameManager.instance.GameOver){
+
+			transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+			if (transform.localPosition.x <= resetPosition) {
+				Vector3 newPos = new Vector3 (startPosition, transform.position.y, transform.position.z);
+				transform.position = newPos;
+			}
+		}
+	}
+}
